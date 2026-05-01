b64443da9a4a09bedda8c226b398fdd675bf780a
DavideCominelli <dvd.cominelli@gmail.com>
2026-05-01 15:38:31 +0200

    first itereation 1.4

--- models/StepOne.py
+++ models/StepOne.py
@@ -294,7 +294,167 @@ class DayAheadTwoPriceBuilder:
             model_name=self.model_name
         )
 
+class RiskAverseBuilder:
+    """Builder for Offering Strategy Under a One-price Balancing Scheme constraints and coefficients for 24 hour"""
+    def __init__(self, model_name="Day-Ahead One-Price Model"):
+        self.P_max = 500 # wind farm installed capacity [MW]
+        self.num_hours = 24
+        self.wind_scenarios = 20
+        self.price_scenarios = 20
+        self.imbalance_scenarios = 4
+        self.num_scenarios = 200
+        self.scenario_list = list(scenarios.values())[:200]
+        self.alpha = 0.90
+        self.beta = 0.95
+        self.model_name = model_name
 
-
+        # Build variable names once
+        self._build_names()
+        
     
+    def _build_names(self):
+        """Build all variable and constraint names"""
+        self.variables = (
+            [f"p_DA_{i+1}" for i in range(self.num_hours)] +
+            [f"delta_{i+1}_{w+1}" for i in range(self.num_hours) for w in range(self.num_scenarios)]+
+            [f"VaR"]+
+            [f"eta_{w+1}" for w in range(self.num_scenarios) ]
+            )
+        
+        self.u_keys = [f"u_p_DA_{t+1}" for t in range(self.num_hours)] 
+        self.l_keys = [f"l_p_DA_{i+1}" for i in range(self.num_hours)]
+        + [f"l_eta_{w+1}" for w in range(self.num_scenarios)]
+        self.balance = [f"bal_{t+1}_{w+1}" for t in range(self.num_hours) for w in range(self.num_scenarios)]
+        self.max_eta = [f"max_eta_{w+1}" for w in range(self.num_scenarios)]
+        
+    
+    def build_objective_coefficients(self):
+        """Build objective coefficients from data.
 
+        Imbalance contribution changes sign with system state:
+        - helps system -> earning (positive coefficient)
+        - hurts system -> cost (negative coefficient)
+        """
+        obj_coeff = {}
+        for hour in range(self.num_hours):
+            obj_coeff[f"p_DA_{hour+1}"] = sum(
+                                        float(s.prices[hour] / self.num_scenarios)
+                                        for s in self.scenario_list
+                                    )
+            count = 0
+            for w in self.scenario_list:
+                da_price = float(w.prices[hour])
+                prob = 1 / self.num_scenarios
+
+                if int(w.imbalance[hour]) == 1:
+                    balancing_price = 1.25 * da_price   # deficit
+                else:
+                    balancing_price = 0.85 * da_price   # surplus (excess)
+                obj_coeff[f"delta_{hour+1}_{count+1}"] = prob * balancing_price
+                count += 1
+        for hour in range(self.num_hours):
+            avg_da = sum(s.prices[hour] / self.num_scenarios for s in self.scenario_list)
+
+            avg_bp = 0
+            for w in self.scenario_list:
+                da_price = float(w.prices[hour])
+                if int(w.imbalance[hour]) == 1:
+                    bp = 1.25 * da_price
+                else:
+                    bp = 0.85 * da_price
+                avg_bp += bp / self.num_scenarios
+
+            print(hour + 1, "E[DA] =", avg_da, "E[BP] =", avg_bp, "E[DA-BP] =", avg_da - avg_bp)
+        for w in self.scenario_list:
+            obj_coeff[f"eta_{w+1}"] = -self.beta * (1 / self.num_scenarios) * (1/(1-self.alpha))
+        obj_coeff["VaR"] = self.beta
+        return obj_coeff
+    
+    def build_constraint_coefficients(self):
+        """Build constraint coefficients"""
+        coeff = {}
+        # capacity: p_DA <= P_max
+        # Upper bounds: x <= max
+        for i in range(self.num_hours):
+            coeff[self.u_keys[i]] = self._one_hot_vector(i, sign=1)
+        # Lower bounds: -x <= 0
+        for i in range(self.num_hours):
+            coeff[self.l_keys[i]] = self._one_hot_vector(i, sign=-1)
+        for w in range(self.num_scenarios):
+            coeff[self.l_keys[self.num_hours + w]] = self._one_hot_vector(self.num_hours + w, sign=-1)
+            
+        # balancing: p_DA + delta = p_real
+        for hour in range(self.num_hours):
+            for w in range(self.num_scenarios):
+                c_name = f"bal_{hour+1}_{w+1}"
+                coeff[c_name] = {
+                    f"p_DA_{hour+1}": 1,
+                    f"delta_{hour+1}_{w+1}": 1
+                }
+        # -Profit + VaR - eta <= 0
+        for w in range(self.num_scenarios):
+            for hour in range(self.num_hours):
+                da_price = float(self.scenario_list[w].prices[hour])
+
+                if int(self.scenario_list[w].imbalance[hour]) == 1:
+                    bp = 1.25 * da_price
+                else:
+                    bp = 0.85 * da_price
+                coeff[f"max_eta_{w+1}"] = {
+                    f"p_DA_{hour+1}": -da_price,
+                    f"delta_{hour+1}_{w+1}": -bp ,
+                    f"VaR": 1,
+                    f"eta_{w+1}": -1}
+        return coeff
+    
+    def build_constraint_rhs(self):
+        """Build right-hand side values"""
+        rhs = {}
+
+        # upper bounds
+        for t in range(self.num_hours):
+            rhs[self.u_keys[t]] = self.P_max
+
+        # lower bounds
+        for t in range(self.num_hours+ self.num_scenarios):
+            rhs[self.l_keys[t]] = 0
+
+        # balance constraints
+        for t in range(self.num_hours):
+            for w in range(self.num_scenarios):
+                rhs[f"bal_{t+1}_{w+1}"] = float(self.scenario_list[w].wind[t])
+        # -Profit + VaR - eta <= 0
+        for w in range(self.num_scenarios):
+            rhs[f"max_eta_{w+1}"] = 0
+
+        return rhs
+    
+    def build_constraint_sense(self):
+        """Build constraint senses"""
+        sense = {}
+        for key in self.u_keys + self.l_keys:
+            sense[key] = GRB.LESS_EQUAL
+        for key in self.balance:
+            sense[key] = GRB.EQUAL
+        for key in self.max_eta:
+            sense[key] = GRB.LESS_EQUAL
+        return sense
+    
+    def _one_hot_vector(self, idx, sign=1):
+        """Helper: create one-hot coefficient vector"""
+        coeff = {v: 0 for v in self.variables}
+        coeff[self.variables[idx]] = sign
+        return coeff
+    
+    def build_input_data(self):
+        """Build complete LP_InputData object"""
+        return LP_InputData(
+            VARIABLES=self.variables,
+            CONSTRAINTS=self.u_keys + self.l_keys + self.balance,
+            objective_coeff=self.build_objective_coefficients(),
+            constraints_coeff=self.build_constraint_coefficients(),
+            constraints_rhs=self.build_constraint_rhs(),
+            constraints_sense=self.build_constraint_sense(),
+            objective_sense=GRB.MAXIMIZE,
+            model_name=self.model_name
+        )
